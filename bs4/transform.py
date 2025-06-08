67a00df9d011ece0821996c6ee6cc52df8fc3911
apisit110 <apisitnowarat@gmail.com>
2025-06-09 01:03:18 +0700

    draft transform

--- bs4/transform.py
+++ bs4/transform.py
@@ -1,90 +1,126 @@
 import os
-import json
-import re
+from datetime import datetime
+import pandas as pd
+# import json
+# import re
 
-_path = os.path.join('bs4', 'libs', 'constants', 'merchant.json')
-f = open(_path, 'r')
-MERCHANT = json.load(f)
-f.close()
+def transformBigC():
+  # ...
+  return
+
+def transformMakroPro():
+  # ...
+  return
+
+def transformWatsons():
+  # ...
+  return
+
+def transformTops():
+  # ...
+  return
+
+def transformLotuss():
+  # ...
+  return
+
+def transformFreshket():
+  fileName = "freshket_product_detail_" + datetime.now().strftime("%Y%m%d") + "_000" + ".csv"
+  filePath = os.path.join("RawZone", fileName)
+  # f = open(filePath, 'r')
+  # # pandas...
+  # f.close()
+  df = pd.read_csv(filePath)
+  # print(df.shape) # return row, column
+  # print(df.head(3)) # จะเรียกมาแค่ 3 แถวแรกเท่านั้น
+  df.info() # ดูชื่อของคอลัมน์ พร้อมประเภทตัวแปร
+
+  return
+transformFreshket()
+
+# _path = os.path.join('bs4', 'libs', 'constants', 'merchant.json')
+# f = open(_path, 'r')
+# MERCHANT = json.load(f)
+# f.close()
 
 
-def mapColumn(row, columnIndex):
-  try:
-    return row.split(',')[columnIndex]
-  except:
-    return ''
+# def mapColumn(row, columnIndex):
+#   try:
+#     return row.split(',')[columnIndex]
+#   except:
+#     return ''
   
-def checkDiscount(priceSale, basePrice):
-  discountPercentage = (100 - ((priceSale / basePrice) * 100))
-  return format(discountPercentage, '.2f')
+# def checkDiscount(priceSale, basePrice):
+#   discountPercentage = (100 - ((priceSale / basePrice) * 100))
+#   return format(discountPercentage, '.2f')
   
-def processEachRow(fileData, groupData):
-  for idx, row in enumerate(fileData.split('\n')):
-    if idx == 0: continue
-    if not row: continue
+# def processEachRow(fileData, groupData):
+#   for idx, row in enumerate(fileData.split('\n')):
+#     if idx == 0: continue
+#     if not row: continue
     
-    date = mapColumn(row, 0)
-    merchant = mapColumn(row, 1)
-    name = mapColumn(row, 2)
-    priceSale = float(mapColumn(row, 3))
-    basePrice = priceSale
-    if mapColumn(row, 4) != '': basePrice = float(mapColumn(row, 4))
-    url = mapColumn(row, 5)
+#     date = mapColumn(row, 0)
+#     merchant = mapColumn(row, 1)
+#     name = mapColumn(row, 2)
+#     priceSale = float(mapColumn(row, 3))
+#     basePrice = priceSale
+#     if mapColumn(row, 4) != '': basePrice = float(mapColumn(row, 4))
+#     url = mapColumn(row, 5)
 
-    discountPercentage = float(checkDiscount(priceSale, basePrice))
-    message = ''
-    prefix = ''
-    percentage = ''
+#     discountPercentage = float(checkDiscount(priceSale, basePrice))
+#     message = ''
+#     prefix = ''
+#     percentage = ''
 
-    if discountPercentage > 0:
-      percentage = f'-{discountPercentage}% '
+#     if discountPercentage > 0:
+#       percentage = f'-{discountPercentage}% '
     
-    if discountPercentage > 10:
-      prefix = '[hot] '
-      pass
+#     if discountPercentage > 10:
+#       prefix = '[hot] '
+#       pass
     
-    message += f'{prefix}{percentage}{name} {url}'
-    if discountPercentage > 10: print(message)
+#     message += f'{prefix}{percentage}{name} {url}'
+#     if discountPercentage > 10: print(message)
 
-    if merchant == MERCHANT['BIG_C']:
-      pass
-    elif merchant == MERCHANT['MAKRO_PRO']:
-      if re.search('คริสตัล น้ำดื่ม 1.5 ล. x 6', name):
-        groupData['pd1']['MAKRO_PRO']['priceSale'] = priceSale
-        groupData['pd1']['MAKRO_PRO']['basePrice'] = basePrice
-      pass
-    elif merchant == MERCHANT['TOPS']:
-      pass
-    elif merchant == MERCHANT['LOTUSS']:
-      if re.search('คริสตัล น้ำดื่ม 1500 มล. แพ็ค 6', name):
-        groupData['pd1']['LOTUSS']['priceSale'] = priceSale
-        groupData['pd1']['LOTUSS']['basePrice'] = basePrice
-      pass
-    elif merchant == MERCHANT['FRESHKET']:
-      pass
+#     if merchant == MERCHANT['BIG_C']:
+#       pass
+#     elif merchant == MERCHANT['MAKRO_PRO']:
+#       if re.search('คริสตัล น้ำดื่ม 1.5 ล. x 6', name):
+#         groupData['pd1']['MAKRO_PRO']['priceSale'] = priceSale
+#         groupData['pd1']['MAKRO_PRO']['basePrice'] = basePrice
+#       pass
+#     elif merchant == MERCHANT['TOPS']:
+#       pass
+#     elif merchant == MERCHANT['LOTUSS']:
+#       if re.search('คริสตัล น้ำดื่ม 1500 มล. แพ็ค 6', name):
+#         groupData['pd1']['LOTUSS']['priceSale'] = priceSale
+#         groupData['pd1']['LOTUSS']['basePrice'] = basePrice
+#       pass
+#     elif merchant == MERCHANT['FRESHKET']:
+#       pass
 
-arr = os.listdir(os.path.join('RawZone'))
+# arr = os.listdir(os.path.join('RawZone'))
 
-for item in sorted(arr, reverse=True):
-  # print(f'process file :: {item}')
+# for item in sorted(arr, reverse=True):
+#   # print(f'process file :: {item}')
 
-  groupData = {
-    'pd1': {
-      'MAKRO_PRO': {
-        'priceSale': '',
-        'basePrice': ''
-      },
-      'LOTUSS': {
-        'priceSale': '',
-        'basePrice': ''
-      },
-    }
-  }
+#   groupData = {
+#     'pd1': {
+#       'MAKRO_PRO': {
+#         'priceSale': '',
+#         'basePrice': ''
+#       },
+#       'LOTUSS': {
+#         'priceSale': '',
+#         'basePrice': ''
+#       },
+#     }
+#   }
 
-  _path = os.path.join('RawZone', item)
-  f = open(_path, 'r')
-  processEachRow(f.read(), groupData)
-  f.close()
+#   _path = os.path.join('RawZone', item)
+#   f = open(_path, 'r')
+#   processEachRow(f.read(), groupData)
+#   f.close()
 
-  # print(groupData)
-  break
+#   # print(groupData)
+#   break
